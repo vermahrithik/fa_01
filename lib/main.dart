6fba1cef4d866d336de1ebc72427858e6900234d
vermahrithik <hackerkernel02.hrithik@gmail.com>
2024-11-12 19:37:43 +0530

    ticker animations : 3d cube implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math' show pi;
 import 'package:flutter/material.dart';
+// import 'package:vector_math/vector_math.dart' as VectorMath;
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-
   late AnimationController _xController;
   late AnimationController _yController;
   late AnimationController _zController;
@@ -43,26 +43,19 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     // TODO: implement initState
     super.initState();
 
-    _xController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 20)
-    );
+    _xController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 20));
 
-    _yController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 30)
-    );
+    _yController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 30));
 
-    _zController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 40)
-    );
+    _zController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 40));
 
     _animation = Tween<double>(
       begin: 0,
-      end: pi*2,
+      end: pi * 2,
     );
-
   }
 
   @override
@@ -76,29 +69,124 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-
+    _xController
+      ..reset()
+      ..repeat();
+    _yController
+      ..reset()
+      ..repeat();
+    _zController
+      ..reset()
+      ..repeat();
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: widthAndHeight,
-              width: double.infinity,
-            ),
-            Stack(
-              children: [
-                Container(
-                  color: Colors.red,
-                  height: widthAndHeight,
-                  width: widthAndHeight,
-                )
-              ],
-            )
-          ],
-        )
-      ),
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: widthAndHeight,
+            width: double.infinity,
+          ),
+          AnimatedBuilder(
+            animation:
+                Listenable.merge([_xController, _yController, _zController]),
+            builder: (context, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..rotateX(_animation.evaluate(_xController))
+                  ..rotateY(_animation.evaluate(_yController))
+                  ..rotateZ(_animation.evaluate(_zController)),
+                child: Stack(
+                  children: [
+                    // back
+                    Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()..translate(0, 0, -widthAndHeight),
+                      child: Container(
+                        height: widthAndHeight,
+                        width: widthAndHeight,
+                        decoration: BoxDecoration(
+                            // color: Colors.purple,
+                            gradient: RadialGradient(colors: [Colors.purple,Colors.transparent]),
+                            border: Border.fromBorderSide(BorderSide(color: Colors.white,width: 1))
+                        ),
+                      ),
+                    ),
+                    // left
+                    Transform(
+                      alignment: Alignment.centerLeft,
+                      transform: Matrix4.identity()..rotateY(pi/2.0),
+                      child: Container(
+                        height: widthAndHeight,
+                        width: widthAndHeight,
+                        decoration: BoxDecoration(
+                          // color: Colors.red,
+                            gradient: RadialGradient(colors: [Colors.red,Colors.transparent]),
+                          border: Border.fromBorderSide(BorderSide(color: Colors.white,width: 1))
+                        ),
+                      ),
+                    ),
+                    // right
+                    Transform(
+                      alignment: Alignment.centerRight,
+                      transform: Matrix4.identity()..rotateY(-pi/2.0),
+                      child: Container(
+                        height: widthAndHeight,
+                        width: widthAndHeight,
+                        decoration: BoxDecoration(
+                          // color: Colors.blue,
+                            gradient: RadialGradient(colors: [Colors.blue,Colors.transparent]),
+                          border: Border.fromBorderSide(BorderSide(color: Colors.white,width: 1))
+                        ),
+                      ),
+                    ),
+                    // front
+                    Container(
+                      height: widthAndHeight,
+                      width: widthAndHeight,
+                      decoration: const BoxDecoration(
+                          // color: Colors.green,
+                          gradient: RadialGradient(colors: [Colors.green,Colors.transparent]),
+                          border: Border.fromBorderSide(BorderSide(color: Colors.white,width: 1))
+                      ),
+                    ),
+                    // top
+                    Transform(
+                      alignment: Alignment.topCenter,
+                      transform: Matrix4.identity()..rotateX(-pi/2.0),
+                      child: Container(
+                        height: widthAndHeight,
+                        width: widthAndHeight,
+                        decoration: BoxDecoration(
+                            // color: Colors.orange,
+                            gradient: RadialGradient(colors: [Colors.orange,Colors.transparent]),
+                            border: Border.fromBorderSide(BorderSide(color: Colors.white,width: 1))
+                        ),
+                      ),
+                    ),
+                    // bottom
+                    Transform(
+                      alignment: Alignment.bottomCenter,
+                      transform: Matrix4.identity()..rotateX(pi/2.0),
+                      child: Container(
+                        height: widthAndHeight,
+                        width: widthAndHeight,
+                        decoration: BoxDecoration(
+                          // color: Colors.brown,
+                            gradient: RadialGradient(colors: [Colors.brown,Colors.transparent,]),
+                            border: Border.fromBorderSide(BorderSide(color: Colors.white,width: 1))
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          )
+        ],
+      )),
     );
   }
 }
